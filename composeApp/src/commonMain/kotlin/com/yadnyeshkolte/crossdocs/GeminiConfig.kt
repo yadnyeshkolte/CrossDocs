ccc6d5fe7dc4b45a4e78d8b74f31248fcfd8402d
yadnyeshkolte <yadnyeshkolte@gmail.com>
2025-01-10 20:16:23 +0530

    update bug fix

--- composeApp/src/commonMain/kotlin/com/yadnyeshkolte/crossdocs/GeminiConfig.kt
+++ composeApp/src/commonMain/kotlin/com/yadnyeshkolte/crossdocs/GeminiConfig.kt
@@ -1,7 +1,7 @@
 package com.yadnyeshkolte.crossdocs.gemini
 
 object GeminiConfig {
-    //const val API_KEY = "AIzaSyDZxmTvnb6WpzMud5UZvurZ66RCWl93KZg" // Replace with your actual API key
+    //const val API_KEY = "" // Replace with your actual API key
 
     const val API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent"
 }
